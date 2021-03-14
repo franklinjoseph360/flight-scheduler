79fd765910e1121f6c689859c774869e5c5c70f0
franklinjoseph360 <franklinjoseph360@gmail.com>
2021-03-15 03:37:05 +0800

    Add error alert, handle error/exception to inital data load of flight data

--- client/Home.jsx
+++ client/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import FlightCard from "./components/FlightCard.jsx";
 import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,21 +16,30 @@ export const Home = () => {
 
     const classes = useStyles();
 
-    const [flights, loadFlightsData] = useState(null);
+    const [flights, loadFlightsData] = useState(false);
+    const [errorMessage, setError] = useState(false);
 
     useEffect(async () => {
         console.log('Load Flights data ...');
         axios.defaults.headers.get['Content-Type'] = 'application/json';
-        axios.get(`/flights`)
+        axios.get(`${process.env.REACT_APP_FLIGHTS_API_ENDPOINT}`)
             .then(async (res) => {
                 console.log('res', res);
-                const flightsData = await res.data;
-                loadFlightsData(flightsData);
+                const flightsData = await res.data.flights;
+                if(flightsData.length > 0) loadFlightsData(flightsData);
+                else throw new Error("No data found")
+            })
+            .catch(err => {
+                console.log('Error loading flights data' + err);
+                setError(err.toString())
             })
     }, [])
 
     return (
         <div className={classes.root}>
+            {errorMessage &&
+                <Alert severity="error" onClose={() => setError(false)}>{errorMessage}</Alert>
+            }
             {flights && flights.map(flightDetail => {
                 return (
                     <FlightCard
